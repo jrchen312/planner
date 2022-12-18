4666766dc86a990dfaeaab1df3a9aca21638f34d
jrchen312 <jrchen@andrew.cmu.edu>
2022-12-17 20:28:24 -0700

    Added initial templating for a models and forms.py.

---
+++ planner/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import Textarea, DateInput, TimeInput, SelectMultiple
+ 
+# from freefood.models import Event
+
+class TrackingBlockForm(forms.Form):
+    start_date = forms.DateField()
+    end_date = forms.DateField()
+
+    name = forms.CharField()
+    color = forms.CharField()
+
+    
+
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your models here.
+
+class Profile(models.Model):
+    user            = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
+    picture         = models.FileField()
+    content_type    = models.CharField(max_length=50)
+
+
+class TrackingBlock(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="tracking_blocks")
+
+    start = models.DateField()
+    end = models.DateField()
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+
+
+class Event(models.Model):
+    block = models.ForeignKey(TrackingBlock, on_delete=models.CASCADE, related_name="events")
+
+    color = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+
+    currently_tracking = models.OneToOneField('CalendarItem', on_delete=models.CASCADE, related_name="+", null=True)
+    
+
+class CalendarItem(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="calendar_items")
+
+    location = models.CharField(blank=True, max_length=200)
+
+    startTime = models.DateField()
+    endTime = models.DateField(null=True)
+
+
